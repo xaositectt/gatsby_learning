87a53603e6fe7cec5d7d09465b5a0c1c3c256bec
xaositectt <xaositectt@gmail.com>
2019-12-13 14:42:24 +0100

    added auth service with hard coded user

---
+++ src/services/auth.js
@@ -0,0 +1,34 @@
+export const isBrowser = () => typeof window !== "undefined"
+
+export const getUser = () => {
+  const user = window.localStorage.getItem("gatsbyUser")
+  if (isBrowser() && user) {
+    return JSON.parse(user)
+  } else {
+    return {}
+  }
+}
+
+const setUser = user => window.localStorage.setItem("gatsbyUser", JSON.stringify(user))
+
+// hard coded data, get the info out of DB and shit later
+export const handleLogin = ({ username, password }) => {
+  if (username === `john` && password === `pass`) {
+    return setUser({
+      username: `john`,
+      name: `Johnny`,
+      email: `johnny@example.org`,
+    })
+  }
+  return false
+}
+
+export const isLoggedIn = () => {
+  const user = getUser()
+  return !!user.username
+}
+
+export const logout = callback => {
+  setUser({})
+  callback()
+}
