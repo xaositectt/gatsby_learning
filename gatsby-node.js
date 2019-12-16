c5d0c7b351df144c873872b45716c20106dec82a
xaositectt <xaositectt@gmail.com>
2019-12-16 14:02:39 +0100

    added generated pages with node-js

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,9 +2,27 @@ exports.onCreatePage = async ({page, actions}) => {
   console.log(page)
   console.log(actions)
   const { createPage } = actions
-  // if (page.path.match(/^\/app/)) {
+  // check route with regex
   //   page.matchPath = "/app/*"
   //   // Update the page.
   //   createPage(page)
-  }
+  // }
+  const dogData = [
+    {
+      name: "Fido",
+      breed: "Sheltie",
+    },
+    {
+      name: "Sparky",
+      breed: "Corgi",
+    }
+  ]
+
+  dogData.forEach(dog => {
+    createPage({
+      path: `/${dog.name}`,
+      component: require.resolve(`./src/templates/dog-template.js`),
+      context: dog
+    })
+  })
 }
